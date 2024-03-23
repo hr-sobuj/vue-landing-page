90b42517871af62728428f0fedfabdde71690fba
hr-sobuj <sobujhd@gmail.com>
2024-03-23 11:52:02 +0600

    home d ts file added

---
+++ src/types/home.d.ts
@@ -0,0 +1,11 @@
+declare module 'home' {
+    interface HomeContent {
+        navContent: any;
+        heroContent: any;
+        growContent: any;
+        securityContent: any;
+    }
+
+    const home: HomeContent;
+    export default home;
+}
